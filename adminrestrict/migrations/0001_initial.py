062f97e0ba04021a74785e85e857314e627f6af2
Simas Skrebiškis <simas.skrebiskis@gmail.com>
2016-12-28 11:58:29 +0200

    migrations

---
+++ adminrestrict/migrations/0001_initial.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='AllowedIP',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('ip_address', models.CharField(max_length=512)),
+            ],
+        ),
+    ]
